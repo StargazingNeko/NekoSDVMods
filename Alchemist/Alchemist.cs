f3e6384cb8d523eef27851d9bc0b17b0fa30909f
StargazingNeko <StargazingNeko@users.noreply.github.com>
2025-08-24 16:26:07 -0700

    Init

---
+++ Alchemist/Alchemist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using xTile.Tiles;
+
+namespace Alchemist
+{
+    public class Alchemist : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            
+        }
+    }
+}
